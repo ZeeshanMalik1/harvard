c8006a5e2e2716ee4ede11b81cda6df4a2841409
moizhassan7 <hassanmoiz.119sb@gmail.com>
2025-10-02 17:48:41 +0500

    updated with gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import GalleryHero from '../components/GalleryHero';
 import GallerySection from '../components/GallerySection';
+
 function Gallery() {
   return (
     <>
-    <GalleryHero/>
-    <GallerySection/>
+      <GalleryHero />
+
+      {/* Wrapper div to center iframe */}
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "20px", alignItems: "center" }}>
+  <iframe
+    src="https://www.facebook.com/plugins/page.php?href=https://www.facebook.com/TheHarvardSchoolSargodhaPakistan&tabs=timeline&width=1000&height=800&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+    style={{ border: "none", overflow: "hidden", width: "100%", maxWidth: "1000px", height: "800px" }}
+    scrolling="no"
+    frameBorder="0"
+    allowFullScreen={true}
+    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+  />
+</div>
+
+
+      {/* <GallerySection/> */}
     </>
   )
 }
